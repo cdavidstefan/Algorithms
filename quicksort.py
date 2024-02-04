ea4a86b200bd0df8c6eb5246be372e1d1a520726
David Ciucă <ciucadavidstefan@gmail.com>
2024-02-04 10:21:15 +0200

    quicksort

---
+++ quicksort.py
@@ -0,0 +1,24 @@
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) // 2]
+    left = []
+    middle = []
+    right = []
+
+    for x in arr:
+        if x < pivot:
+            left.append(x)
+        elif x == pivot:
+            middle.append(x)
+        else:
+            right.append(x)
+
+    return quicksort(left) + middle + quicksort(right)
+
+
+# Example usage:
+arr = [3, 6, 8, 10, 1, 2, 1]
+sorted_arr = quicksort(arr)
+print("Sorted array:", sorted_arr)
