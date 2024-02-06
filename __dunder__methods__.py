268967debb2dde1bdaacb88f28f425bd1d8c76a6
David Ciucă <ciucadavidstefan@gmail.com>
2024-02-06 22:09:59 +0200

    linked-list implementation

---
+++ __dunder__methods__.py
@@ -0,0 +1,88 @@
+class User():
+    def sign_in(self):
+        print('logged in')
+
+class Wizard(User):
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        print(f'Attacking with power of {self.power}')
+
+class Archer(User):
+    def __init__(self, name, range):
+        self.name = name
+        self.range = range
+
+    def my_range(self):
+        print(f'My maximum range is {self.range} meters.')
+
+    def run(self):
+        print('ran really fast')
+
+
+class HybridCharacter(Wizard, Archer):
+    def __init__(self, name, power, range_length):
+        Wizard.__init__(self, name, power)
+        Archer.__init__(self, name, range_length)
+
+hb1 = HybridCharacter('borgie', 50, 500)
+print(hb1.run())
+
+
+
+# MRO - method resolution order is a rule in python that says in which order are things inherited
+
+class A:
+    num = 10
+
+class B(A):
+    pass
+
+class C(A):
+    num = 1
+
+class D(B, C):
+    pass
+
+print(D.num)
+print(D.mro())
+
+
+
+
+
+
+# class Toy():
+#     def __init__(self, color, age):
+#         self.color = color
+#         self.age = age
+#         self.my_dict = {
+#             'name': 'Yoyo',
+#             'has_dog': False
+#         }
+#
+#     def __str__(self):
+#         return f'{self.color}'
+#
+#     def __len__(self):
+#         return 500
+#
+#     def __getitem__(self, i):
+#         return self.my_dict[i]
+#
+#
+#
+# action_figure = Toy('red', 0)
+# print(action_figure.__str__())
+# print(str('action_figure'))
+#
+#
+# print(len(action_figure))
+#
+# arr = [1,2,3]
+#
+# print(len(arr))
+#
+# print(action_figure['name'])
