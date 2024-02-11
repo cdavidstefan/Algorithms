9307aaa3468e259bb9d60f34faa9ad3784e0e3a0
David Ciucă <ciucadavidstefan@gmail.com>
2024-02-11 10:34:38 +0200

    get(key)

--- Data Structures/JavaScript/hash_table_implementation.js
+++ Data Structures/JavaScript/hash_table_implementation.js
@@ -15,18 +15,28 @@ class HashTable {
         let address = this._hash(key);
         if (!this.data[address]) {
             this.data[address] = [];
-            this.data[address].push([key, value]);
-        } else {
-            this.data[address].push([key, value]);
         }
+        this.data[address].push([key, value]);
+        return this.data
     }
 
     get(key) {
-
+        let address = this._hash(key);
+        const currentBucket = this.data[address];
+        console.log(currentBucket);
+        if (currentBucket) {
+            for (i = 0; i < currentBucket.length; i++) {
+                if (currentBucket[i][0] === key) {
+                    return currentBucket[i][1];
+                }
+            }
+        }
+        return undefined;
     }
 }
 
 const myHashTable = new HashTable(50);
 
 
-myHashTable.set('grapes', 10000);
+myHashTable.set('grapes', 100);
+myHashTable.set('apples', 30);
