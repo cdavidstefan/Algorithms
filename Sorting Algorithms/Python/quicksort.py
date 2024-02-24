eaf5473bacb95f8a1375d784e189f7be2f3b394a
David Ciucă <ciucadavidstefan@gmail.com>
2024-02-24 23:15:02 +0200

    quicksort practice

--- Sorting Algorithms/Python/quicksort.py
+++ Sorting Algorithms/Python/quicksort.py
@@ -26,6 +26,32 @@ sorted_arr = quicksort(arr)
 print("Sorted array:", sorted_arr)
 
 
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    midpoint_element = arr[len(arr) // 2]
+    left = []
+    middle = []
+    right = []
+
+    for x in arr:
+        if x < midpoint_element:
+            left.append(x)
+        elif x == midpoint_element:
+            middle.append(x)
+        else:
+            right.append(x)
+
+    return quick_sort(left) + middle + quick_sort(right)
+
+array = [3, 6, 8, 10, 1, 2, 1]
+sorted_array = quick_sort(arr)
+print(sorted_array)
+
+
+
+
 # Using list comprehension:
 
 def quick_sort(arr):
