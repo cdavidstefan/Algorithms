0a18c400574e7ac5b55c85a3f41116aa4723132a
David Ciucă <ciucadavidstefan@gmail.com>
2024-02-24 22:55:16 +0200

    insertion_sort_practice

--- Sorting Algorithms/Python/insertionSort.py
+++ Sorting Algorithms/Python/insertionSort.py
@@ -29,46 +29,29 @@ sorted_array = insertionSort(array)
 print(sorted_array)
 
 
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        j = i - 1
+        current_element = arr[i]
+        while arr[j] > current_element and j >= 0:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            j -= 1
+        arr[j + 1] = current_element
+    return arr
+
+arr = [5, 22232, 2, 43, 21, 676, 99, 91, 554,34534, 0,34, 4]
+sorted_arr = insertion_sort(arr)
+print(sorted_arr)
 
 
 
-# def merge_sort(my_list):
-#     # Incepem sortarea doar daca lista are mai mult de un element.
-#     if len(my_list) > 1:
-#         # Stabilim punctul de mijloc, jumatatea din stanga si jumatatea din dreapta
-#         midpoint = len(my_list) // 2
-#         left = my_list[:midpoint]
-#         right = my_list[midpoint:]
-#
-#         merge_sort(left)
-#         merge_sort(right)
-#
-#         i = 0
-#         j = 0
-#         k = 0
-#
-#         while i < len(left) and j < len(right):
-#             if left[i] < right[j]:
-#                 my_list[k] = left[i]
-#                 i += 1
-#             else:
-#                 my_list[k] = right[j]
-#                 j += 1
-#             k += 1
-#
-#         while i < len(left):
-#             my_list[k] = left[i]
-#             i += 1
-#             k += 1
-#
-#         while j < len(right):
-#             my_list[k] = right[j]
-#             j += 1
-#             k += 1
-#
-#     return my_list
-#
-#
-# my_list = [99, 443, 1621, 24, 83, 1, 75, 25, 9, 22, 10]
-# mergeSortedList = merge_sort(my_list)
-# print(mergeSortedList)
+
+
+
+
+
+
+
+
+
