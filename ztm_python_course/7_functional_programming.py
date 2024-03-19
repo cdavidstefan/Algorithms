51b0a21d3cea366615692051494c0679e98afaf0
David Ciucă <ciucadavidstefan@gmail.com>
2024-03-19 22:35:44 +0200

    filter

--- ztm_python_course/7_functional_programming.py
+++ ztm_python_course/7_functional_programming.py
@@ -2,7 +2,7 @@
 
 # map -------------------------------------------------
 # map(action, iterable)
-my_list = [1,2,3,4,5]
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 def multiply_by_2(item):
     return item * 2
     # new_list = []
@@ -13,3 +13,13 @@ def multiply_by_2(item):
 
 print(list(map(multiply_by_2, my_list)))
 print(my_list)  # my list is not modified.
+
+# filter ----------------------------------------------
+
+
+def only_even(item):
+    return item % 2 == 0
+
+
+print(list(filter(only_even, my_list)))
+
